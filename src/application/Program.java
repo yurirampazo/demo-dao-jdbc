3d63009c2da03b01092f7d9e5c2e989a18068998
Yuri <yuri.rampazo@outlook.com>
2021-08-12 15:52:30 -0300

    Created Seller Entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
@@ -8,6 +11,8 @@ public class Program {
         Department obj = new Department(1, "Books");
         System.out.println(obj);
 
+        Seller seller = new Seller(30, "Bob", "bob@test.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
 
     }
 }
