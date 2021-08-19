1354a694bb5a702783c735c8b5c310aa656df0b0
Yuri <yuri.rampazo@outlook.com>
2021-08-19 16:59:14 -0300

    Created: findById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,14 +10,18 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
+//        Department obj = new Department(1, "Books");
+//        System.out.println(obj);
+//
+//        Seller s = new Seller(3, "Bob", "bob@test.com", new Date(), 3000.0, obj);
+//        System.out.println(s);
 
-        Seller seller = new Seller(30, "Bob", "bob@test.com", new Date(), 3000.0, obj);
-        System.out.println(seller);
 
         // Injeção de dependências sem explicitar a implementação.
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(6);
+        System.out.println(seller);
 
     }
 }
