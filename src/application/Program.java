e49a6ae32325aa66f6fdb809b904af73f6260998
Yuri <yuri.rampazo@outlook.com>
2021-08-25 11:19:48 -0300

    Created: Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,11 +39,17 @@ public class Program {
         }
 
         System.out.println("\n--------Teste 4: seller insert--------");
-
         Seller newSeller = new Seller(null, "Greg",
                 "greg@gmail.com", new Date(),
                 4000.0, department);
                sellerDao.insert(newSeller);
             System.out.println("Inserted! New id: " + newSeller.getId());
+
+
+        System.out.println("\n--------Teste 5: seller update--------");
+        seller = sellerDao.findById(1);
+        seller.setName("Jonas Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update Completed!");
     }
 }
